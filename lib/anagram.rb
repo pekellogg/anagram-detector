4984bd96e29a3dfb7d83059931416c16e47202f4
Peyton <pekellogg@hotmail.com>
2021-09-17 17:12:02 -0700

    commit tree d9f29bf91ed68dec12739bb077592773ccca7bd6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(word)
+        @word = word
+        @@all << self
+    end
+
+    def match(collection)
+        collection.select {|i| i.chars.sort == self.word.chars.sort} 
+    end
+end
